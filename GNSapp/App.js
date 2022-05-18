67b8a2e298d5d8df94cae4e4559c32f9482fa8ab
Thiago Vazquez Enninger <thiagoenninger@gmail.com>
2022-05-18 20:35:32 -0300

    criação input e-mail e senha

--- GNSapp/App.js
+++ GNSapp/App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TextInput, StatusBar } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput style={styles.inputDados} placeholder="E-mail" />
+        <TextInput style={styles.inputDados} placeholder="Senha" />
+        <StatusBar barStyle="light-content" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
+    width: "100%",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    flexDirection: "column",
+  },
+  inputDados: {
+    width: 260,
+    height: 50,
+    backgroundColor: "#e8e8e8",
+    color: "rgb(60, 59, 59)",
+    marginTop: 30,
+    borderRadius: 10,
   },
 });
