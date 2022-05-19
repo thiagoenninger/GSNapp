0e12055f4a6979e71f1bc1539185eabe91f019f6
Thiago Vazquez Enninger <thiagoenninger@gmail.com>
2022-05-18 23:31:30 -0300

    inserido botoes acesso com midias, btn registre-se e área para logo

--- GNSapp/App.js
+++ GNSapp/App.js
@@ -18,6 +18,11 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <View style={styles.containerLogo}>
+          <Text style={{ fontSize: 18, fontWeight: "900", color: "#fff" }}>
+            Logo
+          </Text>
+        </View>
         <View style={styles.section}>
           <Image source={Mail} style={styles.imagemMail} />
           <TextInput
@@ -34,20 +39,43 @@ export default class App extends React.Component {
             underlineColorAndroid="transparent"
           />
         </View>
+
         <Pressable
           style={styles.btnEsqueciSenha}
           onPress={(_) => alert("Página em construção")}
         >
           <Text style={styles.txtEsqueciSenha}>ESQUECI SENHA</Text>
         </Pressable>
+
         <Pressable
           style={styles.btnLogin}
           onPress={(_) => alert("Página em construção")}
         >
           <Text style={styles.txtLogin}>LOGIN</Text>
         </Pressable>
-        <Text style={{ fontSize: 12 }}>ou acesse com:</Text>
-        <View></View>
+        <Text style={{ fontSize: 12, marginBottom: 30 }}>ou acesse com:</Text>
+        <View style={styles.containerMidiasSociais}>
+          <Pressable onPress={(_) => alert("Página em Construção")}>
+            <Image source={Gmail} style={styles.imagemMidias} />
+          </Pressable>
+          <Pressable onPress={(_) => alert("Página em Construção")}>
+            <Image source={Facebook} style={styles.imagemMidias} />
+          </Pressable>
+          <Pressable onPress={(_) => alert("Página em Construção")}>
+            <Image source={Twitter} style={styles.imagemMidias} />
+          </Pressable>
+        </View>
+
+        <View style={styles.containerRegistro}>
+          <Text style={{ fontSize: 18, marginRight: 10 }}>Novo no GSN?</Text>
+          <Pressable onPress={(_) => alert("Página em Construção")}>
+            <Text
+              style={{ fontSize: 18, fontWeight: "bold", color: "#32a060" }}
+            >
+              Registre-se
+            </Text>
+          </Pressable>
+        </View>
       </View>
     );
   }
@@ -58,6 +86,15 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  containerLogo: {
+    width: 260,
+    height: 160,
+    backgroundColor: "#c4c4c4",
+    borderRadius: 30,
+    marginBottom: 50,
+    alignItems: "center",
+    justifyContent: "center",
   },
   section: {
     flexDirection: "row",
@@ -108,11 +145,29 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     width: 260,
     height: 50,
-    marginBottom: 15,
+    marginBottom: 30,
   },
   txtLogin: {
     color: "#fff",
     fontSize: 22,
     fontWeight: "bold",
+  },
+  containerMidiasSociais: {
+    width: 300,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginBottom: 50,
+  },
+  imagemMidias: {
+    padding: 10,
+    height: 50,
+    width: 50,
+    resizeMode: "stretch",
+    alignItems: "center",
+  },
+  containerRegistro: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
