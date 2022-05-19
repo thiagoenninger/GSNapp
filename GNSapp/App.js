25faf24498710b2022d626bd20543cc7eeff70eb
Thiago Vazquez Enninger <thiagoenninger@gmail.com>
2022-05-18 23:03:01 -0300

    finalizado inputs de email e senha, inserido btn esqueci senha e inserido btn login

--- GNSapp/App.js
+++ GNSapp/App.js
@@ -1,13 +1,53 @@
 import React from "react";
-import { StyleSheet, Text, View, TextInput, StatusBar } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  StatusBar,
+  Image,
+  Pressable,
+} from "react-native";
 
+import Mail from "./assets/mail.png";
+import Locker from "./assets/locker.png";
+import Gmail from "./assets/gmail.png";
+import Facebook from "./assets/facebook.png";
+import Twitter from "./assets/twitter.png";
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <TextInput style={styles.inputDados} placeholder="E-mail" />
-        <TextInput style={styles.inputDados} placeholder="Senha" />
-        <StatusBar barStyle="light-content" />
+        <View style={styles.section}>
+          <Image source={Mail} style={styles.imagemMail} />
+          <TextInput
+            style={{ flex: 1, marginLeft: 10 }}
+            placeholder="Email"
+            underlineColorAndroid="transparent"
+          />
+        </View>
+        <View style={styles.section}>
+          <Image source={Locker} style={styles.imagemLocker} />
+          <TextInput
+            style={{ flex: 1, marginLeft: 10 }}
+            placeholder="Senha"
+            underlineColorAndroid="transparent"
+          />
+        </View>
+        <Pressable
+          style={styles.btnEsqueciSenha}
+          onPress={(_) => alert("Página em construção")}
+        >
+          <Text style={styles.txtEsqueciSenha}>ESQUECI SENHA</Text>
+        </Pressable>
+        <Pressable
+          style={styles.btnLogin}
+          onPress={(_) => alert("Página em construção")}
+        >
+          <Text style={styles.txtLogin}>LOGIN</Text>
+        </Pressable>
+        <Text style={{ fontSize: 12 }}>ou acesse com:</Text>
+        <View></View>
       </View>
     );
   }
@@ -15,17 +55,64 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    width: "100%",
     flex: 1,
-    display: "flex",
-    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
   },
-  inputDados: {
+  section: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#e8e8e8",
     width: 260,
     height: 50,
-    backgroundColor: "#e8e8e8",
-    color: "rgb(60, 59, 59)",
-    marginTop: 30,
     borderRadius: 10,
+    marginBottom: 30,
+  },
+  imagemMail: {
+    padding: 10,
+    marginLeft: 10,
+    height: 22,
+    width: 32,
+    resizeMode: "stretch",
+    alignItems: "center",
+  },
+  imagemLocker: {
+    padding: 10,
+    marginLeft: 10,
+    height: 22,
+    width: 32,
+    resizeMode: "stretch",
+    alignItems: "center",
+  },
+  btnEsqueciSenha: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    borderRadius: 25,
+    borderWidth: 0.5,
+    borderColor: "#000",
+    width: 110,
+    height: 25,
+    marginBottom: 30,
+  },
+  txtEsqueciSenha: {
+    color: "#a0a0a0",
+    fontSize: 12,
+    fontWeight: "bold",
+  },
+  btnLogin: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#32a060",
+    borderRadius: 10,
+    width: 260,
+    height: 50,
+    marginBottom: 15,
+  },
+  txtLogin: {
+    color: "#fff",
+    fontSize: 22,
+    fontWeight: "bold",
   },
 });
